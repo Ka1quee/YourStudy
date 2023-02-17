1fa5104da253ba7c225d77840565ed6853937c78
rogerio_000590 <rogerio_000590@BF-P3L4-MPROF.fieb.local>
2023-02-16 22:13:55 -0300

    Camada Model em construção

---
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/model/Role.java
@@ -0,0 +1,12 @@
+package com.fieb.tcc.academicologin.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Role {
+	
+	
+
+}
--- academicologin/src/main/java/com/fieb/tcc/academicologin/model/User.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/model/User.java
@@ -1,6 +1,23 @@
 package com.fieb.tcc.academicologin.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+@Entity
+@Table(name="users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	
+	private String email;
+	private String password;
+	
 }
