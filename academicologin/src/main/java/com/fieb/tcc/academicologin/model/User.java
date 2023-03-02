58502c8ccb092622ebc04b2e306a0d8fa080f151
rogerio_000590 <rogerio_000590@BF-P3L4-MPROF.fieb.local>
2023-03-02 19:24:21 -0300

    Ajustes application.properties

--- academicologin/src/main/java/com/fieb/tcc/academicologin/model/User.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/model/User.java
@@ -3,6 +3,7 @@ package com.fieb.tcc.academicologin.model;
 import java.util.Collection;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -21,7 +22,10 @@ public class User {
 	@Id // Chave Primária
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-Incremento
 	private Long id;
+	
+	@Column(name="first_name")
 	private String firstName;
+	@Column (name="last_name")
 	private String lastName;
 	private String email;
 	private String password;
