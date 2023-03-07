bb50afad095bbdde3a4639b4c8b68aca4a1d2ba0
rogerio_000590 <rogerio_000590@BF-LAB04-MV-M32.fieb.local>
2023-03-06 22:40:40 -0300

    Camada Controller parcialmente implementada

--- academicologin/src/main/java/com/fieb/tcc/academicologin/service/UserService.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/service/UserService.java
@@ -1,6 +1,9 @@
 package com.fieb.tcc.academicologin.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+
 
 public interface UserService extends UserDetailsService {
 
---
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/web/UserController.java
@@ -0,0 +1,19 @@
+package com.fieb.tcc.academicologin.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.fieb.tcc.academicologin.service.UserService;
+
+@Controller
+public class UserController {
+	
+	
+	private UserService userService;
+	
+	public UserController(UserService userService) {
+		
+		this.userService = userService;
+	}
+
+}
