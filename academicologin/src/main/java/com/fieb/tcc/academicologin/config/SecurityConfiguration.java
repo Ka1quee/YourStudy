397aaa46db24bbdf703700b510fb9d1b959b1f53
rogerio_000590 <rogerio_000590@BF-P3L4-MPROF.fieb.local>
2023-03-13 22:44:03 -0300

    Arquivos de front-end. Configuração parcial do thymeleaf

--- academicologin/src/main/java/com/fieb/tcc/academicologin/config/SecurityConfiguration.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,12 +32,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		return auth;
 	}
 	
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(
+				"registration**").permitAll();
 	
-	
+	}
 	
 }
