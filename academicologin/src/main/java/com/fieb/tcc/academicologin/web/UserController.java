23900c35bc16f6bdb9f05256150be0d9fae303b3
rogerio_000590 <rogerio_000590@BF-P3L4-MPROF.fieb.local>
2023-03-13 22:04:45 -0300

    Construído a base para implementar o login como o cadastro de usuário.

--- academicologin/src/main/java/com/fieb/tcc/academicologin/web/UserController.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/web/UserController.java
@@ -2,8 +2,11 @@ package com.fieb.tcc.academicologin.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.fieb.tcc.academicologin.service.UserService;
+import com.fieb.tcc.academicologin.web.dto.UserDto;
 
 @Controller
 public class UserController {
@@ -14,6 +17,16 @@ public class UserController {
 	public UserController(UserService userService) {
 		
 		this.userService = userService;
+	}
+	
+	@GetMapping("/registration")
+	public String showRegistrationForm() {
+		return "registration";
+	}
+	
+	public String registerUserAccount(@ModelAttribute("user") UserDto userDto) {
+		userService.save(userDto);
+		return "redirect:/registration?success";
 	}
 
 }
