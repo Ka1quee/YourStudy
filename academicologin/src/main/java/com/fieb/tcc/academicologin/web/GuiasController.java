285eabbeee7e4500e18a9c3262c754f6c3b6a0e5
Ka1quee <kaiqueoliveira375@gmail.com>
2023-06-01 21:16:00 -0300

    Adcionando paginas da área adminsitrativa

--- academicologin/src/main/java/com/fieb/tcc/academicologin/web/GuiasController.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/web/GuiasController.java
@@ -4,14 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminController {
+public class GuiasController {
 	
-	
-	@GetMapping("/users/admin-home")
-	public String adminHome() {
-		return "admin/admin-home";
+	@GetMapping("/users/guia-cadastro")
+	public String guiaCadastro() {
+		return "admin/guia-cadastro";
 	}
-	
-
 
 }
