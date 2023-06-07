9f6f96b7617f7c53839b19ea2443b8ff3278b0e3
Ka1quee <kaiqueoliveira375@gmail.com>
2023-06-06 22:24:37 -0300

    Atualizando listagem de guias

--- academicologin/src/main/java/com/fieb/tcc/academicologin/web/GuiasController.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/web/GuiasController.java
@@ -13,13 +13,10 @@ import com.fieb.tcc.academicologin.repository.GuiaRepository;
 
 @Controller
 public class GuiasController {
-	
-	
+
 	@Autowired
 	private GuiaRepository guiarepositorio;
 
-	
-	
 	@GetMapping("/users/guia-cadastro")
 	public ModelAndView cadastrarGuia(Guia guia) {
 		ModelAndView mv = new ModelAndView();
@@ -28,60 +25,83 @@ public class GuiasController {
 		return mv;
 
 	}
-	
+
 	@PostMapping("inserirGuias")
-	public ModelAndView inserirGuias(Guia guia) { 
-		
+	public ModelAndView inserirGuias(Guia guia) {
 		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/users/guia-list");
+		guiarepositorio.save(guia);
 
-
-			mv.setViewName("redirect:/users/guias"); 
-													
-			guiarepositorio.save(guia);
-		
-	
 		return mv;
 	}
-	
+
 	// listagem de guias
-	
-	
+
 	@GetMapping("/users/guias")
 	public ModelAndView listagemGuias() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("conteudo/guias-list");
-		mv.addObject("guiaList", guiarepositorio.findAll()); 
-		
+		mv.addObject("guiaList", guiarepositorio.findAll());
+
 		return mv;
 	}
-	@GetMapping("/users/guia")
-	public String guiaEstudo() {
-		return "conteudo/guia-conteudo";
-	} 
-	
-	// Fazer método pra entrar nos cards dos guias
-	
-	
-	
-	
 
 // listagem dos guias em tabela
-	
+
 	@GetMapping("/users/guia-list")
 	public ModelAndView guiaList() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("conteudo/listagem-de-guias");
-		mv.addObject("guiaList", guiarepositorio.findAll()); 
-		
+		mv.addObject("guiaList", guiarepositorio.findAll());
+
+		return mv;
+	}
+
+	// exclusao dos cadastros dos guias
+
+	@GetMapping("/users/excluir-guia/{id}")
+	public String excluirGuia(@PathVariable("id") Long id) {
+		guiarepositorio.deleteById(id); 
+										
+		return "redirect:/users/guia-list";
+	}
+
+	// editar cadastros
+
+	@GetMapping("/users/editar-guia/{id}")
+	public ModelAndView editarGuia(@PathVariable("id") Long id) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("admin/guia-editar");
+		Guia guia = guiarepositorio.getOne(id);
+		mv.addObject("guia", guia); 
+									
+		return mv;
+	}
+
+	// postagem das alterações
+
+	@PostMapping("/editar-guia")
+	public ModelAndView alterar(Guia guia) {									
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/users/guia-list"); 
+		guiarepositorio.save(guia);
+
 		return mv;
 	}
 	
-	//exclusao dos cadastros dos guias
 	
-	@GetMapping("/users/excluir-guia/{id}")
-	public String excluirGuia(@PathVariable("id") Long id) {
-		guiarepositorio.deleteById(id); // dessa vez usamos o metodo de delete ao inves de save, dessa maneira ele
-											// irá deletar o usuario buscando pelo seu id
-		return "redirect:/users/guia-list";
+	
+	// conteudo cadastrado no guia
+	
+	@GetMapping("/users/guia/{id}")
+	public ModelAndView conteudoGuia(@PathVariable("id") Long id) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("conteudo/guia-conteudo");
+		Guia guia = guiarepositorio.getOne(id);
+		mv.addObject("guia", guia); 
+									
+		return mv;
 	}
+	 
+
 }
