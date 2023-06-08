bf099d0965d04d6983419a35ff4b6abb8e79cd28
Kaique <kaiqueoliveira375@gmail.com>
2023-06-08 14:24:52 -0300

    Atualizando interface do aluno e admin

--- academicologin/src/main/java/com/fieb/tcc/academicologin/web/GuiasController.java
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/web/GuiasController.java
@@ -2,7 +2,7 @@ package com.fieb.tcc.academicologin.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
